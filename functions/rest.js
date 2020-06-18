2bd6a69b21381631f97c516890fefaf568583017
uhmseohun <uhmtotow@gmail.com>
2020-06-18 10:39:34 +0900

    feat: add record routes

--- functions/rest.js
+++ functions/rest.js
@@ -1,9 +1,18 @@
+const { RecordModel } = require('../models')
+
 exports.getRoomMembers = (io) => (req, res, next) => {
   const { roomName } = req.params;
   const members = io.sockets.adapter.rooms[roomName];
   res.json({ members });
 };
 
-exports.getLeaderboard = (req, res, next) => {};
+exports.getLeaderboard = async (req, res, next) => {
+  const records = await RecordModel.find({}).sort('-time').limit(10)
+  res.json({ records })
+};
 
-exports.postLeaderboard = (req, res, next) => {};
+exports.createLeaderboard = async (req, res, next) => {
+  const { name, time } = req.body;
+  const newRecord = await RecordModel.create({ name, time });
+  res.json({ record: newRecord });
+}
