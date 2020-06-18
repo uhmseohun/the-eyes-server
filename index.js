a67160712f1884d1307e5b74c36a58bcbf5da55d
uhmseohun <uhmtotow@gmail.com>
2020-06-18 10:39:46 +0900

    feat: implement record route and mongoose

--- index.js
+++ index.js
@@ -2,15 +2,24 @@ const app = require('express')();
 
 const { createServer } = require('http');
 const server = createServer(app);
+const bodyParser = require('body-parser');
 
 const socket = require('socket.io');
 const io = socket(server);
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/the-eyes', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 app.all('/*', (_, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'X-Requested-With');
   next();
 });
+app.use(bodyParser.json());
 
 const {
   onJoinRoom,
@@ -25,12 +34,12 @@ io.on('connection', (socket) => {
 const {
   getRoomMembers,
   getLeaderboard,
-  postLeaderboard,
+  createLeaderboard,
 } = require('./functions/rest');
 
 app.get('/room/:roomName/members', getRoomMembers(io));
 app.get('/leaderboard', getLeaderboard);
-app.post('/leaderboard', postLeaderboard);
+app.post('/leaderboard', createLeaderboard);
 
 server.listen(3000, () => {
   console.log('server is running on *:3000');
